b02ec29a3b8f74153be2da3c372349470e9e427e
Wlodek Bzyl <matwb@univ.gda.pl>
2012-11-09 19:26:44 +0100

    scaffold: bez specs dla widoków i metod pomocniczych

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,11 @@ module ProjektZespolowy
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.generators do |g|
+      g.view_specs false
+      g.helper_specs false
+    end
+
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
